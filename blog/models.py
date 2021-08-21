b187c6c08dc5b17a397a91ad4144caedb6fe601f
Lei <gabriel_sl19798@hotmail.com>
2021-08-21 23:46:09 +1200

    Modelling, Template, genericview, Form, Ordering

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+from datetime import datetime,date
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255)
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + ' | '+ str(self.auther)
+
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
+
